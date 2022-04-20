dd647de700c2b14fabba07a95c755066e7901fda
ricardo.fedrigo <ricardofedrigo1995@gmail.com>
2022-04-20 17:15:32 -0300

    Update screen

--- lib/view/GameScreen.dart
+++ lib/view/GameScreen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:letroca_clone_flutter/Components/CountDownTimer.dart';
 import 'package:letroca_clone_flutter/view/KeyboardScreen.dart';
 
-//import 'WordsToFindScreen.dart';
+import 'WordsToFindInScreen.dart';
 
 class GameScreen extends StatelessWidget {
   const GameScreen({Key? key}) : super(key: key);
@@ -44,9 +44,9 @@ class GameScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             Container(
-                alignment: Alignment.topCenter,
-                child: null //WordsToFindScreen(),
-                ),
+              alignment: Alignment.topCenter,
+              child: WordsToFindInScreen(),
+            ),
             Container(
                 alignment: Alignment.bottomCenter, child: KeyboardScreen())
           ],
